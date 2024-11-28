f93f61848e6f22f9ef63df53ab6f070071f29910
JordanLippert <lippertx11@gmail.com>
2024-11-28 19:53:10 -0300

    feat: new full-screen color

--- apps/web/src/components/full-screen-page.tsx
+++ apps/web/src/components/full-screen-page.tsx
@@ -6,7 +6,7 @@ export function FullScreenPage({
   className,
 }: PropsWithChildren<{ className?: string }>) {
   return (
-    <main className={cn('w-full min-h-screen h-full p-4 bg-emerald-500', className)}>
+    <main className={cn('w-full min-h-screen h-full p-4 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-zinc-400 via-teal-700 to-stone-400', className)}>
       {children}
     </main>
   );
